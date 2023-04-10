0f84c48745c04490d456ad89124ad4f1200f595e
felomae <felomae.anhaw@g.msuiit.edu.ph>
2023-04-11 06:43:46 +0800

    Configured for Lab3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-auth.js";
+import { getAuth } from "firebase/auth";
+import { getFirestore, collection, addDoc, connectFirestoreEmulator, query, getDocs } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyAmcVWoiOB3esn01BajQgz2B5zCb-KvSvA",
@@ -16,6 +17,32 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
+// const analytics = getAnalytics(app);
+const auth = getAuth();
+const db = getFirestore();
+connectFirestoreEmulator(db, 'localhost', 8080);
 console.log(app)
+
+// const saveBtn = document.querySelector(".save")
+// saveBtn.addEventListener("click", async () =>{
+// try {
+//   const collectionRef = collection(db, "user")
+//   const newUser = await addDoc(collectionRef,{
+//     name: "Felomae",
+//     surname: "Anhaw",
+//     email: "felomae.anhaw@gmail.com"
+//   })
+//   console.log(`Create new user: ${newUser.id}`)
+// } catch (error) {
+//   console.log(error)
+// }
+// })
+
+// const getDataBtn = document.querySelector(".get-data")
+// getDataBtn.addEventListener("click", async () =>{
+//   const q = query(collection(db,"user"))
+//   const user = await getDocs(q)
+//   user.forEach((user) => {
+//     console.log(user.data())
+//   })
+// })
